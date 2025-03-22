9e57794d2cc69f9e906fa7c27d5fa30b8eab75ba
Andrei Baraitaru <andrei.bara08@gmail.com>
2025-03-22 15:29:25 +0100

    good version predicting career correctly v1

--- test_ai.py
+++ test_ai.py
@@ -7,34 +7,20 @@ model = joblib.load(os.path.join(script_dir, "../recommender-models/career_xgb.p
 scaler = joblib.load(os.path.join(script_dir, "../recommender-models/scaler.pkl"))
 label_encoder = joblib.load(os.path.join(script_dir, "../recommender-models/label_encoder.pkl"))
 
+# Example test data engineered to lean towards "Software Engineer"
 test_data = {
-    "GPA": 2.5,
-    "Extracurriculars": "No",
-    "InternshipExperience": "Yes",
-    "Projects": 5,
-    "Leadership_Positions": 2,
-    "Courses": "Yes",
-    "Research_Experience": 4,
-    "Coding_Skills": 5,
-    "Communication_Skills": 1,
-    "Problem_Solving_Skills": 5,
-    "Teamwork_Skills": 3,
-    "AnalyticalSkills": 5,
-    "Presentation_Skills": 1,
-    "Networking_Skills": 1,
-    "Certifications": "Yes"
+    "math_score": 95,
+    "history_score": 70,
+    "physics_score": 96,
+    "chemistry_score": 85,
+    "biology_score": 78,
+    "english_score": 88,
+    "geography_score": 75
 }
 
 features = pd.DataFrame([test_data])
-
-categorical_columns = ["Extracurriculars", "InternshipExperience", "Courses", "Certifications"]
-for col in categorical_columns:
-    features[col] = features[col].map({"Yes": 1, "No": 0})
-
 features_scaled = scaler.transform(features)
 predicted_label = model.predict(features_scaled)[0]
 
-# to decode label to career name
 predicted_career = label_encoder.inverse_transform([predicted_label])[0]
-
 print(f"🎯 Recommended Career: {predicted_career}")
