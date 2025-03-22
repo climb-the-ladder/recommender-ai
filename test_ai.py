34e7be1051fbd4a77fd96b1e208a25d1fab8ea24
Andrei Baraitaru <andrei.bara08@gmail.com>
2025-03-22 16:00:49 +0100

    multiple tests, all predicted correctly

--- test_ai.py
+++ test_ai.py
@@ -9,13 +9,52 @@ label_encoder = joblib.load(os.path.join(script_dir, "../recommender-models/labe
 
 # Example test data engineered to lean towards "Software Engineer"
 test_data = {
-    "math_score": 95,
-    "history_score": 70,
-    "physics_score": 96,
-    "chemistry_score": 85,
-    "biology_score": 78,
-    "english_score": 88,
-    "geography_score": 75
+
+    #software engineer:
+    #"math_score": 95,
+    #"history_score": 70,
+    #"physics_score": 96,
+    #"chemistry_score": 85,
+    #"biology_score": 78,
+    #"english_score": 88,
+    #"geography_score": 75
+
+    #doctor:
+    # "math_score": 80,
+    # "history_score": 68,
+    # "physics_score": 88,
+    # "chemistry_score": 95,
+    # "biology_score": 94,  # High bio
+    # "english_score": 82,
+    # "geography_score": 65
+
+    #real estate developer:
+    # "math_score": 50,
+    # "history_score": 92,
+    # "physics_score": 45,
+    # "chemistry_score": 60,
+    # "biology_score": 70,
+    # "english_score": 95,
+    # "geography_score": 88
+
+    #lawyer
+    # "math_score": 70,
+    # "history_score": 90,   # Strong in history
+    # "physics_score": 60,
+    # "chemistry_score": 65,
+    # "biology_score": 70,
+    # "english_score": 95,   # Strong English
+    # "geography_score": 80
+
+    #scientist
+    "math_score": 92,
+    "history_score": 75,
+    "physics_score": 94,
+    "chemistry_score": 96,
+    "biology_score": 89,
+    "english_score": 80,
+    "geography_score": 78
+
 }
 
 features = pd.DataFrame([test_data])
